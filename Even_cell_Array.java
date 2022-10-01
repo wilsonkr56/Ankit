25baf7396783d32784ebdea682250f2dc6112d1e
AjeebAadmi <57108152+AjeebAadmi@users.noreply.github.com>
2022-10-02 00:16:47 +0530

    Update Even_cell_Array.java

--- Even_cell_Array.java
+++ Even_cell_Array.java
@@ -8,8 +8,8 @@ BufferedReader in=new BufferedReader(read);
 int a[]=new int[8];
 int i;
 System.out.println("Enter the number in the cell");
-for(i=0;i<8;i++)
-a[i]=Integer.parseInt(in.readLine());
+for(i=0 ;i<8 ;i++)
+a[i] = Integer.parseInt(in.readLine());
 for(i=0;i<8;i++)
 if(i%2==0)
 System.out.println(a[i]);
