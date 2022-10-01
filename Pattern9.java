33be37ab093a35d60b1354c8d3e21da8865d632d
AjeebAadmi <57108152+AjeebAadmi@users.noreply.github.com>
2022-10-02 00:15:25 +0530

    Update Pattern9.java

--- Pattern9.java
+++ Pattern9.java
@@ -9,9 +9,9 @@ String st;
 System.out.println("Enter the word for pattern");
 st=in.next();
 b=st.length();
-for(a=b;a>=0;a--)
+for(a=b; a>=0; a--)
 {
-for(p=1;p<=k;p++)
+for(p=1; p<=k; p++)
 {
 System.out.println(st.substring(0,a));
 k++;
