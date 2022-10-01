282e82b6dd4935f9550df6ea14dbd4de429d848c
AjeebAadmi <57108152+AjeebAadmi@users.noreply.github.com>
2022-10-02 00:11:05 +0530

    Update Max_Min.java

--- Max_Min.java
+++ Max_Min.java
@@ -15,12 +15,12 @@ a[i]=Integer.parseInt(in.readLine());
 max=a[0];min=a[0];
 for(i=0;i<10;i++)
 {
-if(a[i]>max)
+if(a[i] > max)
 max=a[i];
 if(a[i]<min)
 min=a[i];
 }
-System.out.println("The greatest element is"+max);
-System.out.println("The smallest element is"+min);
+System.out.println("The greatest element is "+max);
+System.out.println("The smallest element is "+min);
 }
 }
